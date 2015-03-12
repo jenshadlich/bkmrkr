3b824d93b8ebd64775450faeddfb145869cad016
jenshadlich <jenshadlich@googlemail.com>
2015-03-12 23:19:39 +0100

    use templates

--- src/bkmrkr.go
+++ src/bkmrkr.go
@@ -1,16 +1,25 @@
 package main
 
 import (
-    "io"
+    "html/template"
+    "log"
     "net/http"
 )
 
+type Page struct {
+    Title string
+}
+
 func index(w http.ResponseWriter, r *http.Request) {
-    io.WriteString(w, "bkmrkr")
+    log.Println(r.Header)
+    t, _ := template.ParseFiles("templates/index.html")
+    t.Execute(w, &Page{Title: "bkmrkr"})
 }
 
 func main() {
     mux := http.NewServeMux()
     mux.HandleFunc("/", index)
+
+    log.Println("Listening...")
     http.ListenAndServe(":8000", mux)
 }
